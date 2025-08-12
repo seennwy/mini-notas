f52c57e32da16639b238bb2a2e6e642fa81f56f7
David <davidcasadocruz56@gmail.com>
2025-08-12 20:03:32 +0200

    ajuste api

---
+++ src/env.d.ts
@@ -0,0 +1,10 @@
+/// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly SUPABASE_URL: "https://puwbmsavutwyeeryaskd.supabase.co";
+  readonly SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB1d2Jtc2F2dXR3eWVlcnlhc2tkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUwMTgyNTYsImV4cCI6MjA3MDU5NDI1Nn0.ds3Pv-SnxNOrXQfOIFtZ_1I_n5un443VoKt-aIMBroY";
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
 
-import { createClient } from '@supabase/supabase-js'
+const supabaseUrl = import.meta.env.SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.SUPABASE_ANON_KEY;
 
-const supabaseUrl = 'https://puwbmsavutwyeeryaskd.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB1d2Jtc2F2dXR3eWVlcnlhc2tkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUwMTgyNTYsImV4cCI6MjA3MDU5NDI1Nn0.ds3Pv-SnxNOrXQfOIFtZ_1I_n5un443VoKt-aIMBroY'
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
