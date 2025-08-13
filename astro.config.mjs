baf4ee3636f637e03302baefb4949c33886516c8
David <davidcasadocruz56@gmail.com>
2025-08-13 19:02:05 +0200

    js login separado

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,5 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
 
-export default defineConfig({
-  vite: {
-    define: {
-      'import.meta.env.SUPABASE_URL': JSON.stringify(process.env.SUPABASE_URL),
-      'import.meta.env.SUPABASE_ANON_KEY': JSON.stringify(process.env.SUPABASE_ANON_KEY),
-    }
-  }
-});
+// https://astro.build/config
+export default defineConfig({});
